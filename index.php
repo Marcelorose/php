8c1ebac28db7ccd782c25c938bd51e1dee8e16f7
Marcelo Rose <marcelorose@edu.univali.br>
2018-05-03 15:43:31 -0300

    resolvido o conflito(atualizado)

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 
-<<<<<<< HEAD
 	echo "Oi 321";
 
-	echo "Ola";
-=======
-      echo "Oi 1";
->>>>>>> eb3c3f5490fd96347af5e07e5487fcb5352f3ea9
+	echo "Ola 2";    
 
-      echo "Oi 2";
-      
 ?>
